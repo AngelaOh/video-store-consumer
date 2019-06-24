1237652522baca19a26ed8a05b2318317f3ab93b
Angela <angela.ohh@gmail.com>
2019-06-24 15:47:38 -0700

    able to view list of movies with MovieLibrary and Movie components

--- src/component/MovieLibrary.js
+++ src/component/MovieLibrary.js
@@ -1,12 +1,60 @@
-// testest
 import React from 'react'
+import Movie from './Movie'
+import axios from 'axios';
 
+class MovieLibrary extends React.Component {
 
-const MovieLibrary = (props) => {
-    return (
-        <p>hello</p>
-    )
+    constructor() {
+        super();
+        this.state = {
+            movieList: []
+        }
+    }
+
+    componentDidMount() {
+        URL = 'http://localhost:3000'
+
+        axios.get( URL + '/movies' ) 
+        .then((response) => {
+            const movieList = response.data.map((movie) => {
+                return (
+                    movie
+                )
+            })
+    
+            this.setState ({
+                movieList: movieList
+            })
+
+            console.log('state!', this.state)
+        })
+        .catch((error) => {
+            return (
+                console.log(error.message)
+            )
+        })
+
+    }
+
+    movieCollection = () => {
+        return (
+        this.state.movieList.map((movie) => {
+            return (
+                // console.log('movietitle', movie.title)
+                < Movie movie={movie} />
+            )
+        })
+        )
+    }
+
+    render() {      
+
+        return (
+            <div>
+              {this.movieCollection()}
+            </div>
+        )
+    }
 }
-
 
 export default MovieLibrary 
