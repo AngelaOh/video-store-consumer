3bc44e375fa3334f6d7de6dbe36f8cccc4cfada7
Angela <angela.ohh@gmail.com>
2019-06-25 11:19:20 -0700

    added a get request to Rails API in MovieSearch

--- src/component/MovieSearch.js
+++ src/component/MovieSearch.js
@@ -1,7 +1,30 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
+
+const getMovies = () => {
+  URL = 'http://localhost:3000'
+  axios.get( URL + '/movies' ) 
+  .then((response) => {
+      const movieList = response.data.map((movie) => {
+          return movie
+      });
+
+      this.setState ({
+          movieList: movieList
+      });
+  })
+  .catch((error) => {
+      return console.log(error.message);
+  });
+};
 
 const MovieSearch = () => {
-  return <h1>MOVIE SEARCH PLACE HOLDER</h1>;
+  return (
+    <div>
+      <h1>MOVIE SEARCH PLACE HOLDER</h1>
+      {getMovies()}
+    </div>
+  )
 };
 
 export default MovieSearch;
