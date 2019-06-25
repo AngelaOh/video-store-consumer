acaad7a8f049b98a608f02832c32c15d6132b444
Angela <angela.ohh@gmail.com>
2019-06-25 13:37:37 -0700

    sets state so SearchedMovies returns all movies from external database based on search query

--- src/component/MovieSearch.js
+++ src/component/MovieSearch.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
-import Axios from 'axios';
+import axios from 'axios';
+import Movie from './Movie'
 
-const getMovies = () => {
+class MovieSearch extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      searchTitle: '',
+      searchedMovies: undefined
+    }
+  }
+
+getMovies = () => {
   URL = 'http://localhost:3000'
-  axios.get( URL + '/movies' ) 
+  console.log('inside axios', this.state.searchTitle)
+  axios.get( URL + '/movies?query=' + this.state.searchTitle ) 
   .then((response) => {
       const movieList = response.data.map((movie) => {
           return movie
       });
 
       this.setState ({
-          movieList: movieList
+          searchedMovies: movieList
       });
   })
   .catch((error) => {
@@ -18,13 +29,54 @@ const getMovies = () => {
   });
 };
 
-const MovieSearch = () => {
-  return (
-    <div>
-      <h1>MOVIE SEARCH PLACE HOLDER</h1>
-      {getMovies()}
-    </div>
-  )
+  onMovieInput = (event) => {
+    const updatedSearch = {}
+    updatedSearch[event.target.name] = event.target.value
+    this.setState(updatedSearch)
+  }
+
+  findMovie = (event) => {
+    event.preventDefault();
+    this.getMovies()
+    console.log('inside find movie', this.state.searchedMovies) 
+
+  }
+
+  movieCollection = () => {
+    console.log('inside movieCollection: ', this.state.searchedMovies)
+    return (
+    this.state.searchedMovies.map((movie) => {
+        return (
+            < Movie 
+            key={movie.id}
+            {...movie} />
+        )
+    })
+    )
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>MOVIE SEARCH PLACE HOLDER</h1>
+
+        <form onSubmit={this.findMovie}>
+          <label htmlFor='searchTitle'>Movie Search: </label>
+           <input  
+            name="searchTitle" 
+            onChange={this.onMovieInput} />
+          
+          <input
+            type='submit'
+            name='submit'
+            value='Search Movie' />
+        </form>
+
+        {this.searchedMovies && this.movieCollection()}
+
+      </div>
+    )
+  }
 };
 
 export default MovieSearch;
