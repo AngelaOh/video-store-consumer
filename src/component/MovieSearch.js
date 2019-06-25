d6080da52839a2ec5ccf72a733e81b277741bc62
qqdipps <sdipp007@gmail.com>
2019-06-25 16:18:17 -0700

    merge resolved

--- src/component/MovieSearch.js
+++ src/component/MovieSearch.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import Movie from './Movie'
+import Movie from './Movie';
 
 class MovieSearch extends React.Component {
   constructor() {
@@ -8,80 +8,72 @@ class MovieSearch extends React.Component {
     this.state = {
       searchTitle: '',
       searchedMovies: undefined,
-      isSearch: true
-    }
+      isSearch: true,
+    };
   }
 
-getMovies = () => {
-  URL = 'http://localhost:3000'
-  console.log('inside axios', this.state.searchTitle)
-  axios.get( URL + '/movies?query=' + this.state.searchTitle ) 
-  .then((response) => {
-      const movieList = response.data.map((movie) => {
-          return movie
+  getMovies = () => {
+    URL = 'http://localhost:3000';
+    console.log('inside axios', this.state.searchTitle);
+    axios
+      .get(URL + '/movies?query=' + this.state.searchTitle)
+      .then(response => {
+        const movieList = response.data.map(movie => {
+          return movie;
+        });
+
+        this.setState({
+          searchedMovies: movieList,
+        });
+      })
+      .catch(error => {
+        return console.log(error.message);
       });
+  };
 
-      this.setState ({
-          searchedMovies: movieList
-      });
-  })
-  .catch((error) => {
-      return console.log(error.message);
-  });
-};
+  onMovieInput = event => {
+    const updatedSearch = {};
+    updatedSearch[event.target.name] = event.target.value;
+    this.setState(updatedSearch);
+  };
 
-  onMovieInput = (event) => {
-    const updatedSearch = {}
-    updatedSearch[event.target.name] = event.target.value
-    this.setState(updatedSearch)
-  }
-
-  findMovie = (event) => {
+  findMovie = event => {
     event.preventDefault();
-    this.getMovies()
-   
-
-  }
+    this.getMovies();
+  };
 
   movieCollection = () => {
-    console.log('inside movieCollection: ', this.state.searchedMovies)
-    return (
-      this.state.searchedMovies.map((movie) => {
-          return (
-              < Movie 
-              key={movie.id}
-              {...movie}
-              isSearch={this.state.isSearch}
-              addToLibrary={this.props.addToLibrary} />
-          )
-      })
-      )
-  }
+    console.log('inside movieCollection: ', this.state.searchedMovies);
+    return this.state.searchedMovies.map(movie => {
+      return (
+        <Movie
+          key={movie.id}
+          {...movie}
+          isSearch={this.state.isSearch}
+          addToLibrary={this.props.addToLibrary}
+        />
+      );
+    });
+  };
 
   render() {
-    console.log('inside render', this.state.searchedMovies) 
+    console.log('inside render', this.state.searchedMovies);
 
     return (
       <div>
         <h1>MOVIE SEARCH PLACE HOLDER</h1>
 
         <form onSubmit={this.findMovie}>
-          <label htmlFor='searchTitle'>Movie Search: </label>
-           <input  
-            name="searchTitle" 
-            onChange={this.onMovieInput} />
-          
-          <input
-            type='submit'
-            name='submit'
-            value='Search Movie' />
+          <label htmlFor="searchTitle">Movie Search: </label>
+          <input name="searchTitle" onChange={this.onMovieInput} />
+
+          <input type="submit" name="submit" value="Search Movie" />
         </form>
 
         {this.state.searchedMovies && this.movieCollection()}
-
       </div>
-    )
+    );
   }
-};
+}
 
 export default MovieSearch;
